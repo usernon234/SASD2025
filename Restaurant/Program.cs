a43bb9375d54ca67045feb1121f40238b5f413dc
DELL <DELL@DESKTOP-NELOF1J>
2026-01-09 15:38:39 +0700

    Add project files.

---
+++ Restaurant/Program.cs
@@ -0,0 +1,21 @@
+namespace Restaurant
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  CHANATIP SUDSOM ID 234
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            if (args.Length > 0 && args[0].ToLower() == "-k")
+                Application.Run(new KitchenForm());
+            else
+                Application.Run(new WaiterForm());
+        }
+    }
+}
